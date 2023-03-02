668ca614c7bdf4d90cab493b1dccd8f486be5838
MakisChristou <makis_christou@protonmail.com>
2023-03-02 19:27:51 +0200

    Basic idea working

---
+++ src/args.rs
@@ -0,0 +1,14 @@
+use clap::Parser;
+
+/// A vanity slatepack address generator
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    /// Desired pattern
+    #[arg(short, long, default_value_t = String::from("grin1234"))]
+    pub pattern: String,
+
+    /// Threads
+    #[arg(short, long, default_value_t = 1)]
+    pub threads: usize,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,89 @@
-
+use grin_core::global;
+use grin_core::global::ChainTypes;
 use grin_keychain;
 use grin_keychain::keychain::ExtKeychain;
 use grin_keychain::Keychain;
-use rand::Rng;
+use grin_util::ToHex;
+use grin_wallet_libwallet;
 use grin_wallet_libwallet::SlatepackAddress;
-use grin_util::secp::key::SecretKey;
-use grin_core::global;
-use grin_core::global::ChainTypes;
+use rand::Rng;
+use std::time::Instant;
+use clap::Parser;
+use std::process;
+use std::thread;
+
+use ed25519_dalek::PublicKey as edDalekPublicKey;
+use ed25519_dalek::SecretKey as edDalekSecretKey;
+
+// Measures how many s elapsed since the given instant
+fn time_since(instant: Instant) -> f64 {
+    return (instant.elapsed().as_nanos() as f64) / 1_000_000_000f64;
+}
+
+mod args;
+use args::Args;
 
 fn main() {
 
-    let mut rng = rand::thread_rng();
-    
-    let mut bytes = Vec::new();
+    let args = Args::parse();
 
-    for _i in 0..24{
-        let random_byte = rng.gen::<u8>();
-        bytes.push(random_byte);
+    // Chosen or default settings
+    println!("Searching for pattern {}", args.pattern);
+    println!("Using {} threads", args.threads);
+
+    if !args.pattern.starts_with("grin1") {
+        println!("Pattern needs to start with grin1");
+        process::exit(0x1);
     }
 
-    let seed = grin_keychain::mnemonic::from_entropy(&bytes).unwrap();
+    // let mut rng = rand::thread_rng();
 
-    let mut keychain = ExtKeychain::from_seed(&bytes, false).unwrap();
+    // let mut bytes = Vec::new();
 
-    let test = keychain.pub_root_key();
+    // for _i in 0..24 {
+    //     let random_byte = rng.gen::<u8>();
+    //     bytes.push(random_byte);
+    // }
+
+    // let seed = grin_keychain::mnemonic::from_entropy(&bytes).unwrap();
+
+    // let mut keychain = ExtKeychain::from_seed(&bytes, false).unwrap();
+
+    // let test = keychain.pub_root_key().public_key;
 
     // println!("{}", seed);
     // println!("{:?}", test);
 
+
     grin_core::global::set_local_chain_type(ChainTypes::Mainnet);
 
+    let mut i = 0;
+    let start_time = Instant::now();
 
-    let test1 = SlatepackAddress::random();
+    loop {
+        let loop_time = Instant::now();
+        let bytes: [u8; 32] = rand::thread_rng().gen();
+        let pub_key = edDalekPublicKey::from(&edDalekSecretKey::from_bytes(&bytes).unwrap());
+        let address = SlatepackAddress::new(&pub_key);
 
-    println!("{}", test1);
+        if i % 50000 == 0 {
+            println!(
+                "{} keys per second",
+                1. / time_since(loop_time)
+            );
+        }
 
+        if address.to_string().starts_with(&args.pattern) {
+            println!(
+                "\nFound address: {} \nPrivate Key:   0x{} \n{} keys in {} seconds",
+                address.to_string(),
+                edDalekSecretKey::from_bytes(&bytes).unwrap().to_hex().to_string(),
+                i,
+                time_since(start_time)
+            );
+            process::exit(0x0);
+        }
+
+        i += 1;
+    }
 }
